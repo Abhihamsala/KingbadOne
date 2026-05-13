a3a98e7f750d5267d4eb4d0b41c7da64d2b7220a
Abhihamsala <Abhihamsala@gmail.com>
2026-05-13 10:41:21 -0500

    java project in Abhibarnch

---
+++ src/test/java/Devpratice/Javasample.java
@@ -0,0 +1,13 @@
+package Devpratice;
+
+public class Javasample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bhargava Hamsala");
+		System.out.println("welcome Vasavi");
+		System.out.println("Bhargava Hamsala");
+		System.out.println("welcome Vasavi");
+	}
+
+}
