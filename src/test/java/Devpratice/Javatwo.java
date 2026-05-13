2880ef3f35576ad1d764781a55633965cff6ef38
Bhargava Hamsala <Bhargava.hamsala@gmail.com>
2026-05-12 19:23:58 -0500

    push two java file

---
+++ src/test/java/Devpratice/Javatwo.java
@@ -0,0 +1,12 @@
+package Devpratice;
+
+public class Javatwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bhargava Hamsala");
+		System.out.println("welcome Vasavi");
+
+	}
+
+}
